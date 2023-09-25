1c69aba2ad08a2d087042bf8699903d91c6ac48d
cosminbware <114251752+cosminbware@users.noreply.github.com>
2023-09-25 11:40:11 +0300

    Update extraRpcs.js
    Added Blast API mainned endpoint on Linea

--- constants/extraRpcs.js
+++ constants/extraRpcs.js
@@ -3361,6 +3361,11 @@ export const extraRpcs = {
         tracking: "none",
         trackingDetails: privacyStatement.drpc,
       },
+      {
+        url: "https://linea-mainnet.public.blastapi.io",
+        tracking: "limited",
+        trackingDetails: privacyStatement.blastapi,
+      },
     ],
   },
   534351: {
